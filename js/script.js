8e530e785a36b22a211167c0a0dd634c706c5eb1
Manuela DAgosto <mdagostom@gmail.com>
2023-05-22 22:29:40 -0300

    primer entregable

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// BIENVENIDA AL CLIENTE
+
+const nombre = prompt(` Buenas! Por favor, contanos como te llamas :) `);
+
+console.log(`El nombre del cliente es: ${nombre}`);
+
+const msjBienvenida = alert(
+  `Bienvenidx ${nombre} a la pagina oficial de ALFAJORES CHIHIRO! Si queres realizar un pedido, por favor presiona ACEPTAR`
+);
+
+alert(
+  "En este local de alfajores, vos ingresas cuanto dinero queres gastar, y nosotros te decimos que es lo mas caro que te podes comprar y cuánto tenes de vuelto ;) "
+);
+
+// INGRESO DEL DINERO
+
+let dinero = parseInt(prompt(`¿Cuánto estas dispuesto a gastar?`));
+
+console.log("El dinero que tiene el cliente es $" + dinero);
+
+//CALCULADORA PARA AVERIGUAR LA COMPRA Y EL VUELTO
+
+if (dinero >= 250 && dinero < 350) {
+  alert(
+    `Podes comprar UN ALFAJOR DE VAINILLA y tenes de vuelto $` +
+      vuelto(dinero - 250)
+  );
+} else if (dinero >= 350 && dinero < 400) {
+  alert(
+    `Podes comprar UN ALFAJOR DE VAINILLA BAÑADO EN CHOCOLATE y tenes de vuelto $` +
+      (dinero - 350)
+  );
+} else if (dinero >= 400 && dinero < 500) {
+  alert(
+    `Podes comprar UN ALFAJOR DE CHOCOLATE BAÑADO EN CHOCOLATE y tenes de vuelto $` +
+      (dinero - 400)
+  );
+} else if (dinero >= 500 && dinero < 800) {
+  alert(`Podes comprar DOS ALFAJORES y tenes de vuelto $` + (dinero - 500));
+} else if (dinero >= 800 && dinero < 1000) {
+  alert(`Podes comprar CUATRO ALFAJORES y tenes de vuelto $` + (dinero - 800));
+} else if (dinero >= 1000 && dinero < 1500) {
+  alert(
+    `Podes comprar MEDIA DOCENA DE ALFAJORES y tenes de vuelto $` +
+      (dinero - 1000)
+  );
+} else if (dinero >= 1500) {
+  alert(
+    `Podes comprar UNA DOCENA DE ALFAJORES y tenes de vuelto $` +
+      (dinero - 1000)
+  );
+} else {
+  alert(
+    `$` + dinero + ` no es suficiente dinero, por favor volve con mas dinero :)`
+  );
+}
+
+alert(
+  `MUCHAS GRACIAS POR COMPRAR EN ALFAJORES CHIHIRO, te esperamos pronto :) <3 `
+);
